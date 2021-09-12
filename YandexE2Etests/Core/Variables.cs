0bd8a52c8dc7870bfea988dcf7c7acd8725c2b1a
Philip <dem.fil30@gmail.com>
2021-09-13 00:45:20 +0300

    adding files

---
+++ YandexE2Etests/Core/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexE2Etests.Core
+{
+    class Variables
+    {
+        public const string parisCity = "Париж";
+        public const string londonCity = "Лондон";
+    }
+}
---
+++ YandexE2Etests/Pages/LanguagePage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexE2Etests.Pages
+{
+    class LanguagePage
+    {   
+       public readonly By openDropDownBtnLocOnLanguagePage = By.XPath("//*[@class='button select__button button_theme_normal button_arrow_down button_size_m i-bem button_js_inited']");   
+       public readonly By enLangBtnLoc = By.XPath("//*[@class='select__list']/div[6]"); 
+       public readonly By saveLangBtnLocOnLanguagePage =By.XPath("//*[@type='submit']");  
+    }
+}
